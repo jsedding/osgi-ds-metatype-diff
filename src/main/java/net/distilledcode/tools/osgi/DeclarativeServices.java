7e713fa96baddc274a24e31df05c9fefc3c7789a
Julian Sedding <julian.sedding@code-distillery.net>
2019-02-06 10:51:15 +0100

    #8 - Exception in thread main java.lang.RuntimeException: org.apache.felix.scr.impl.parser.ParseException: Exception during parsing
    - improve error message to include which XML file is affected

--- src/main/java/net/distilledcode/tools/osgi/DeclarativeServices.java
+++ src/main/java/net/distilledcode/tools/osgi/DeclarativeServices.java
@@ -16,11 +16,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 import static net.distilledcode.tools.osgi.BundleInvocationHandler.getJarFileAsBundle;
 
@@ -34,18 +33,18 @@ public class DeclarativeServices {
     public static Map<String, ComponentMetadata> readComponentMetadata(JarFile jarFile) throws IOException {
         String serviceComponents = jarFile.getManifest().getMainAttributes().getValue(SERVICE_COMPONENT);
         return Arrays.stream(serviceComponents.split(","))
-                .map(jarFile::getEntry)
-                .map(toInputStream(jarFile).andThen(toComponentMetadata(jarFile)))
+                .map(jarFile::getJarEntry)
+                .map(toComponentMetadata(jarFile))
                 .flatMap(Collection::stream)
                 .peek(m -> m.validate(SCR_LOGGER))
                 .flatMap(component -> generateNames(component).map(p -> new AbstractMap.SimpleEntry<>(p, component)))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    private static Function<InputStream, List<ComponentMetadata>> toComponentMetadata(final JarFile jarFile) {
-        return stream -> {
-            try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+    private static Function<JarEntry, List<ComponentMetadata>> toComponentMetadata(final JarFile jarFile) {
+        return zipEntry -> {
+            try (InputStream inputStream = jarFile.getInputStream(zipEntry)) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
                 XmlHandler handler = new XmlHandler(
                         getJarFileAsBundle(jarFile),
                         SCR_LOGGER,
@@ -57,24 +56,7 @@ public class DeclarativeServices {
                 parser.parseXML(handler);
                 return handler.getComponentMetadataList();
             } catch (Exception e) {
-                throw new RuntimeException(e);
-            } finally {
-                try {
-                    stream.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        };
-
-    }
-
-    private static Function<ZipEntry, InputStream> toInputStream(final ZipFile zipFile) {
-        return jarEntry -> {
-            try {
-                return zipFile.getInputStream(jarEntry);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeException("Error while parsing '" + zipEntry.getName() + "' in '" + jarFile.getName() + "'", e);
             }
         };
     }
